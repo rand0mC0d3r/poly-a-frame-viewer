febaa4a17e851b8a29f38fbf86b7d8a4b80bd124
Bogdan Dumitrescu <practiseo@gmail.com>
2020-04-13 15:36:41 +0200

    added the mix-icon library and field adornments

--- src/components/PolySearch/PolySearch.js
+++ src/components/PolySearch/PolySearch.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 import TextField from '@material-ui/core/TextField';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+import ComposedIcon from 'material-ui-mix-icon';
 
 import PolyResult from '../PolyResult/PolyResult';
 
@@ -72,11 +75,23 @@ class PolySearch extends Component {
     return (
       <div className="PolySearch__container">
         <h2>Google POLY & A-Frame</h2>
+
         <TextField
           autoFocus={true}
           fullWidth
           label="Api Key..."
           variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <ComposedIcon
+                    position="bottom-end"
+                    size="medium"
+                    icon='keyboard'
+                    extraIcon='key'/>
+              </InputAdornment>
+            ),
+            }}
           onChange={(event) => this._addApiKey(event.target.value)}/>
         <a
           className="PolySearch__getKeyLink"
@@ -89,6 +104,17 @@ class PolySearch extends Component {
           disabled={apiKey === null}
           fullWidth
           label="Search for an asset .."
+                    InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <ComposedIcon
+                    position="bottom-end"
+                    size="medium"
+                    icon='book-open'
+                    extraIcon='filter'/>
+              </InputAdornment>
+            ),
+            }}
           variant="outlined"
           onChange={(event) => this._performSearch(event.target.value)}/>
         <div className="PolySearch__thumbnailContainer">
